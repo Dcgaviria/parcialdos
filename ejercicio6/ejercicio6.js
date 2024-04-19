5b2a15125316fbac6e4881ba9b986d0bad646491
Diana_morales <dianacarolina.morales82@gmail.com>
2024-04-18 19:36:31 -0500

    ejercicios

---
+++ ejercicio6/ejercicio6.js
@@ -0,0 +1,12 @@
+
+/*Dado el siguiente array: let frutas = ['manzana', 'plátano', 'pera',
+'naranja'], utiliza el método asociado a los arrays para eliminar el primer
+elemento, el último elemento e inserta al inicio del array el valor de nueva-fruta */
+let frutas = ['manzana', 'plátano', 'pera', 'naranja'];
+
+frutas.shift();
+frutas.pop();
+
+frutas.unshift('nueva-fruta');
+console.log(frutas);
+
---
+++ ejercicio8/ejercicio8.js
@@ -0,0 +1,15 @@
+/*Escribe un código que sume todos los números pares del 1 al 100 utilizando un ciclo
+while y luego imprima el resultado en la consola. */
+
+let suma = 0;
+let i = 0;          
+    while (i <= 100)                                   
+    {
+        if (i % 2 == 0)
+        {
+            suma = suma + i;
+        }
+        i++;
+    }
+    console.log(suma);
+    
